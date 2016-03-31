b01858ac8e1c1aaeb837e0a55f430fb4db02b001
Lee Yurim <fnladl72@naver.com>
2016-03-31 16:11:21 +0900

    PlanClass

--- Calculate_Rate/src/main/Plan.java
+++ Calculate_Rate/src/main/Plan.java
@@ -6,7 +6,21 @@ public class Plan {
 	Gold gold;
 	Silver silver;
 	public Plan(User user){
+		if(user.getType().equals("GOLD")){
+			gold = new Gold();
+			this.excessminute_rate = gold.getExcessminute_rate();
+			this.includedMinutes = gold.getIncludedMinutes();
+			this.additional_rate = gold.getAdditional_rate();
+			this.planrate = gold.getPlanrate();
+		}else if(user.getType().equals("SILVER")){
+			silver = new Silver();
+			this.excessminute_rate = silver.getExcessminute_rate();
+			this.includedMinutes = silver.getIncludedMinutes();
+			this.additional_rate = silver.getAdditional_rate();
+			this.planrate = silver.getPlanrate();
+		}
 	}
+
 	public double getExcessminute_rate() {
 		return excessminute_rate; //추가시간에대한 요금
 	}
@@ -19,4 +33,4 @@ public class Plan {
 	public double getPlanrate() {
 		return planrate;
 	}
-	
+}	
