116ca1e45910f428410baa6296f793f0923e4b9f
petitesize <petitesize22@gmail.com>
2016-03-31 15:40:16 +0900

    TotalField

--- Calculate_Rate/src/main/TotalRate.java
+++ Calculate_Rate/src/main/TotalRate.java
@@ -1,3 +1,27 @@
 public class TotalRate {
-	
+	private User userer user;
+	private Plan plan;
+	private double basic_monthly_rate;
+	private double excessminute;
+	private double familyrate;
+	static private double basic_family_rate;
+	private double additionalrate;
+	private double totalrate;
+
+	public TotalRate(User user) {
+
+	} 
+	private void setCalculate() {
+
+	}
+	public double getTotalrate() {
+	}
+	public double getExcessMinuteRate() {
+	}
+	public double getAdditionalLineRate() {
+	}
+	public double getFamilyrate() {
+	}
+	public double getPlanRate() {
+	}
 }
