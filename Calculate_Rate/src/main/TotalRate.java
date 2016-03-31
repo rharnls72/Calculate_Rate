25692e67d6cb61f5c828a58492eca8829707038f
Lee Yurim <fnladl72@naver.com>
2016-03-31 16:29:53 +0900

    DeleteUnused

--- Calculate_Rate/src/main/TotalRate.java
+++ Calculate_Rate/src/main/TotalRate.java
@@ -1,10 +1,8 @@
 public class TotalRate {
 	private User userer user;
 	private Plan plan;
-	private double basic_monthly_rate;
 	private double excessminute;
 	private double familyrate;
-	static private double basic_family_rate;
 	private double additionalrate;
 	private double totalrate;
 
