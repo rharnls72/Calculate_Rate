b77e71ac9ce79995475e8c3524ba3c8f84ae2434
HongJiYeong <hongj910@gmail.com>
2016-03-31 16:29:18 +0900

    Gold filed

--- Calculate_Rate/src/main/Gold.java
+++ Calculate_Rate/src/main/Gold.java
@@ -1,20 +1,20 @@
 public class Gold {
-	private double excessminute_rate = 0.45;
-	private double includedMinutes = 1000;
-	private double additional_rate = 14.50;
-	private double planrate = 49.95;
+	private double g_excessminute_rate = 0.45;
+	private double g_includedMinutes = 1000;
+	private double g_additional_rate = 14.50;
+	private double g_planrate = 49.95;
 
 	double getExcessminute_rate() {
-		return excessminute_rate;
+		return g_excessminute_rate;
 	}
 	public double getIncludedMinutes() {
-		return includedMinutes;
+		return g_includedMinutes;
 	}
 	public double getAdditional_rate() {
-		return additional_rate;
+		return g_additional_rate;
 	}
 	public double getPlanrate() {
-		return planrate;
+		return g_planrate;
 	}
 
 }
