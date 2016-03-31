cdcde27774e644cdfa358b7ce2fcec7a1b75f948
Lee Yurim <fnladl72@naver.com>
2016-03-31 15:44:30 +0900

    GoldField

--- Calculate_Rate/src/main/Gold.java
+++ Calculate_Rate/src/main/Gold.java
@@ -1,3 +1,20 @@
 public class Gold {
+	private double excessminute_rate = 0.45;
+	private double includedMinutes = 1000;
+	private double additional_rate = 14.50;
+	private double planrate = 49.95;
+
+	double getExcessminute_rate() {
+		return excessminute_rate;
+	}
+	public double getIncludedMinutes() {
+		return includedMinutes;
+	}
+	public double getAdditional_rate() {
+		return additional_rate;
+	}
+	public double getPlanrate() {
+		return planrate;
+	}
 
 }
